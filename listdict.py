5b3796b866d0a1a65432a89dbaf8674392438fe6
Cody Doucette <doucette@bu.edu>
2023-02-01 21:25:18 -0500

    Add list dictionary instructions

---
+++ listdict.py
@@ -0,0 +1,10 @@
+class ListDictionary:
+    def __init__(self):
+        # implement the constructor
+        pass
+
+    def insert(self, key, value):
+        # implement the insert() method
+        return False
+
+    # implement the rest of the dictionary methods below
