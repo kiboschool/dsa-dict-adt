0c59c5177f5f9f21f1d4ddd696d05451fbd43970
Cody Doucette <doucette@bu.edu>
2023-03-14 21:20:38 -0400

    Add solution

--- listdict.py
+++ listdict.py
@@ -1,10 +1,43 @@
 class ListDictionary:
     def __init__(self):
-        # implement the constructor
-        pass
+        self.keys = []
+        self.values = []
 
     def insert(self, key, value):
-        # implement the insert() method
+        if key in self.keys:
+            # overwrite existing value
+            self.values[self.keys.index(key)] = value
+            return True
+
+        # add new key and value
+        self.keys.append(key)
+        self.values.append(value)
         return False
 
-    # implement the rest of the dictionary methods below
+    def remove(self, key):
+        if key not in self.keys:
+            return False
+
+        idx = self.keys.index(key)
+        self.keys = self.keys[:idx] + self.keys[idx + 1:]
+        self.values = self.values[:idx] + self.values[idx + 1:]
+        return True
+
+    def contains(self, key):
+        return key in self.keys
+
+    def lookup(self, key):
+        if key in self.keys:
+            return self.values[self.keys.index(key)]
+
+        # key not in dictionary
+        return None
+
+    def get_keys(self):
+        return self.keys
+
+    def get_values(self):
+        return self.values
+
+    def size(self):
+        return len(self.keys)
--- test_listdict.py
+++ test_listdict.py
@@ -8,4 +8,66 @@ class TestListDictionary(unittest.TestCase):
         # overwriting a key
         assert not ldict.insert(1, "hello")
 
-    # add more unit tests below
+    def test_remove(self):
+        ldict = ListDictionary()
+        ldict.insert(1, "hello")
+        ldict.insert(2, "world")
+        ldict.insert(3, "lorem")
+
+        assert not ldict.remove(5)
+        assert ldict.remove(2)
+        assert ldict.remove(3)
+        assert ldict.remove(1)
+
+    def test_contains(self):
+        ldict = ListDictionary()
+        ldict.insert(1, "hello")
+        ldict.insert(2, "world")
+        ldict.insert(3, "lorem")
+
+        assert ldict.contains(2)
+        assert not ldict.contains(4)
+        ldict.remove(2)
+        assert not ldict.contains(2)
+
+    def test_lookup(self):
+        ldict = ListDictionary()
+        ldict.insert(1, "hello")
+        ldict.insert(2, "world")
+        ldict.insert(3, "lorem")
+
+        assert ldict.lookup(1) == "hello"
+        assert ldict.lookup(2) == "world"
+        assert ldict.lookup(3) == "lorem"
+        assert ldict.lookup(4) == None
+
+    def test_keys(self):
+        ldict = ListDictionary()
+        ldict.insert(1, "hello")
+        ldict.insert(2, "world")
+        ldict.insert(3, "lorem")
+
+        keys = ldict.get_keys()
+        for k in [1, 2, 3]:
+            assert k in keys
+
+    def test_values(self):
+        ldict = ListDictionary()
+        ldict.insert(1, "hello")
+        ldict.insert(2, "world")
+        ldict.insert(3, "lorem")
+
+        values = ldict.get_values()
+        for v in ["hello", "world", "lorem"]:
+            assert v in values
+
+    def test_size(self):
+        ldict = ListDictionary()
+        assert ldict.size() == 0
+        ldict.insert(1, "hello")
+        ldict.insert(2, "world")
+        ldict.insert(3, "lorem")
+
+        assert ldict.size() == 3
+        ldict.remove(2)
+        assert ldict.size() == 2
